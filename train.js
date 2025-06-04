38c19998c1dbf368667effe4f659b7a423e24269
jamshid98 <jamshid98991213@gmail.com>
2025-06-04 23:57:10 +0900

    feat: task1 done

--- train.js
+++ train.js
@@ -1,34 +1,32 @@
-console.log("Jack Ma maslahatlari");
-const list = [
-    "yaxshi talaba boling", //0-20
-    "togri boshliq tanlang va koproq xato qiling", //20-30
-    "ozingiz ishlashni boshlang", //30-40
-    "siz kuchli bolgan narsalarni qiling", //40-50
-    "yoshlarga investitsiya qiling", //50-60
-    "endi dam oling, foydasi yoq", //60+
-];
+// A -task
+let word = "obodonlashtiraolmaganimizda";
+let letter = "a";
+
+function countLetter(word, letter) {
+    let count = 0;
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] === letter) {
+            count++;
+        }
+    }
+    console.log(`${word} da "${letter}" harfi ${count} ta`); 
+}
+countLetter(word, letter); 
+countLetter(word, "o");
+countLetter(word, "i");
+countLetter("Jaloliddin", "d");
+
+        // console.log("Jack Ma maslahatlari");
+// const list = [
+//     "yaxshi talaba boling", //0-20
+//     "togri boshliq tanlang va koproq xato qiling", //20-30
+//     "ozingiz ishlashni boshlang", //30-40
+//     "siz kuchli bolgan narsalarni qiling", //40-50
+//     "yoshlarga investitsiya qiling", //50-60
+//     "endi dam oling, foydasi yoq", //60+
+// ];
 //callback
- function maslahatBering(a, callback) {
-     if (typeof a !== "number") callback("insert a number", null);
-     else if(a <= 20) callback(null,list[0]);
-     else if(a > 20 && a <= 30) callback(null,list[1]);
-     else if(a > 30 && a <= 40) callback(null,list[2]);
-     else if(a > 40 && a <= 50) callback(null,list[3]);
-     else if(a > 50 && a <= 60) callback(null,list[4]);
-     else {
-         setInterval(function(){
-             callback(null, list[5]);
-         }, 1000);
-     }
- }
- console.log("passed here 0");
- maslahatBering(65, (err, data) => {
-     if(err) console.log('ERROR:', err);
-     else {
-          console.log("javob:",data) 
-     }
- });
- console.log("passed here 1");
+ //
 
 // async function maslahatBering(a) {
 //     if (typeof a !== "number") throw new Error("insert a number");
