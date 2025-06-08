d3858765a7efe7f95de3a8155d2dda3d406818a5
jamshid98 <jamshid98991213@gmail.com>
2025-06-08 19:14:38 +0900

    feat: Crud operatsiyalari

--- app.js
+++ app.js
@@ -27,11 +27,29 @@ app.set("view engine", "ejs");
 // Routing code
 app.post("/create-item", (req, res) => {
   // TODO: code with db here
-   //res.send("Yangi item yaratildi!");
+  console.log("user entered /create-item");
+   console.log(req.body);
+   const new_reja = req.body.reja;
+   db.collection("plans").insertOne({ reja: new_reja}, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end("something went wrong");
+    } else {
+      res.end("succcessfully added");
+    } 
+   });
 });
 
 app.get("/", function (req, res) {
-  res.render("reja");
+  console.log("user entered /");
+  db.collection("plans").find().toArray((err, data) => {
+    if (err) {
+      console.log(err);
+      res.end("something went wrong");
+    } else {
+       res.render("reja", {items: data});
+    }
+  }); 
 });
 
 // Serverni ishga tushirish
