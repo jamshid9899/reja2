ba951dd73e162ee581843d2561f8945cbfec362f
jamshid98 <jamshid98991213@gmail.com>
2025-06-08 17:43:09 +0900

    fix: refactoring

---
+++ app.js
@@ -0,0 +1,38 @@
+console.log("Web serverni boshlash");
+const express = require("express");
+const app = express();
+const fs = require("fs");
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if(err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data)
+  }
+});
+
+// MongoDB connect
+// 1 Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// 2 Session
+
+// 3 View code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// Routing code
+app.post("/create-item", (req, res) => {
+  // TODO: code with db here
+   //res.send("Yangi item yaratildi!");
+});
+
+app.get("/", function (req, res) {
+  res.render("reja");
+});
+
+// Serverni ishga tushirish
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,34 +1,11 @@
-console.log("Web serverni boshlash");
-
-const express = require("express");
 const http = require("http");
-const app = express();
+const app = require("./app");
 
-// 1 Kirish code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-// 2 Session (agar kerak bo‘lsa keyin yoziladi)
 
-// 3 View code
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-// Routing code
-app.post("/create-item", (req, res) => {
-  // TODO: code with db here
-  res.send("Yangi item yaratildi!");
-});
-
-app.get("/", function (req, res) {
-  res.render("reja2");
-});
-
-// Serverni ishga tushirish
 const server = http.createServer(app);
 let PORT = 3000;
-
 server.listen(PORT, function () {
-  console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
+  console.log(
+    `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
 });
