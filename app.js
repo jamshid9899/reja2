bb68fd55e0bac9a77653504550ad5c93a62f364e
jamshid98 <jamshid98991213@gmail.com>
2025-06-11 00:46:19 +0900

    feat: FrontEnd Jsni qurish

--- app.js
+++ app.js
@@ -26,23 +26,18 @@ app.set("view engine", "ejs");
 
 // Routing code
 app.post("/create-item", (req, res) => {
-  // TODO: code with db here
-  console.log("user entered /create-item");
+   console.log("user entered /create-item");
    console.log(req.body);
    const new_reja = req.body.reja;
    db.collection("plans").insertOne({ reja: new_reja}, (err, data) => {
-    if (err) {
-      console.log(err);
-      res.end("something went wrong");
-    } else {
-      res.end("succcessfully added");
-    } 
-   });
-});
+   res.json(data.ops[0]);
+  });
+}); 
 
 app.get("/", function (req, res) {
   console.log("user entered /");
-  db.collection("plans").find().toArray((err, data) => {
+  db.collection("plans")
+  .find().toArray((err, data) => {
     if (err) {
       console.log(err);
       res.end("something went wrong");
