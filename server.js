4c1a6e605737b2dba5019d46f5db5487d84d802b
jamshid98 <jamshid98991213@gmail.com>
2025-06-14 21:58:39 +0900

    fix: localhost:3004

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ mongodb.connect(connectionString, {
         module.exports = client
         const app = require("./app");
         const server = http.createServer(app);
-        let PORT = 3000;
+        let PORT = 3004
+        ;
         server.listen(PORT, function () {
             console.log(
                 `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
